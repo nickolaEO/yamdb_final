2b61439b02142f697ada378b57359ab277334b87
Nickolay Evsin <nickolay.evsin@yandex.ru>
2022-05-17 23:07:59 +0300

    troubles with SECRET_KEY

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -11,7 +11,8 @@ DOTENV_PATH = os.path.join(os.path.dirname(BASE_DIR), 'infra', '.env')
 load_dotenv(dotenv_path=DOTENV_PATH)
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+# SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
