db35a3f6e5acfc6573989b104a8bee051513261c
Nickolay Evsin <nickolay.evsin@yandex.ru>
2022-05-18 16:08:33 +0300

    for first review, 502 bad gateway fixes

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -11,9 +11,11 @@ DOTENV_PATH = os.path.join(os.path.dirname(BASE_DIR), 'infra', '.env')
 load_dotenv(dotenv_path=DOTENV_PATH)
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
